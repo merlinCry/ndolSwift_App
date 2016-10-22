2c9f4178af2977803ad281da6bdd552c5f08f160
0102 <songqijian@8dol.com>
2016-10-22 15:39:25 +0800

    Initial Commit

---
+++ ndolSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ndolSwift
+//
+//  Created by song on 16/10/22.
+//  Copyright © 2016年 song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
